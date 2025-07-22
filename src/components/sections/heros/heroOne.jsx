0307330f546e925077a32a4e8883d60cfa6ec010
GenixOne <ronaldjames6548@gmail.com>
2025-07-22 14:48:24 +0500

    commit

--- src/components/sections/heros/heroOne.jsx
+++ src/components/sections/heros/heroOne.jsx
@@ -1,22 +1,31 @@
 // src/components/sections/heros/heroOne.jsx
+'use client';
+
 import Link from 'next/link';
 import InputScreen from '../InputScreen';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function HeroOne() {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <div className="hero-section-2">
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
             <div className="hero-content-2">
-              <div className="sub-title-2" data-animation="fade-zoom-in" data-delay={0.4}>
+              <div className="sub-title-2" data-aos="fade-zoom-in" data-aos-delay="400">
                 <p>Tiktokio</p>
               </div>
-              <h2 data-animation="fade-up">Tiktok Downloader</h2>
-              <h3 className="sub-title-2" data-animation="fade-up" data-delay={0.2}>
+              <h2 data-aos="fade-up">Tiktok Downloader</h2>
+              <h3 className="sub-title-2" data-aos="fade-up" data-aos-delay="200">
                 Download TikTok Video Without Watermark Tiktok to mp4
               </h3>
-              <div className="short-prompt" data-animation="fade-zoom-in">
+              <div className="short-prompt" data-aos="fade-zoom-in">
                 <ul>
                   <li>
                     <Link className="hover-bg" href="/portfolio">
